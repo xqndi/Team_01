a79c4c5ec2254d4d71ec8344254a296c52a04e91
John Tusha <john.tusha@student.tugraz.at>
2021-04-21 11:14:02 +0200

    COFFEE-005 Created Tests and removed setter logic [JT, TB]

--- app/src/main/java/at/tu/graz/coffee/model/Review.kt
+++ app/src/main/java/at/tu/graz/coffee/model/Review.kt
@@ -1,18 +1,3 @@
 package at.tu.graz.coffee.model
 
-class Review {
-    var taste: Int = 0
-    set(value) {
-        if (value in 0..10) field = value
-    }
-    var cost: Int = 0
-        set(value) {
-            if (value in 0..10) field = value
-        }
-    var availability:Int=0
-        set(value) {
-            if (value in 0..10) field = value
-        }
-
-
-}
+data class Review (var taste:Int=0, var cost:Int=0, var availability:Int=0)
---
+++ app/src/test/java/at/tu/graz/coffee/ReviewTest.kt
@@ -0,0 +1,31 @@
+package at.tu.graz.coffee
+
+import at.tu.graz.coffee.model.Review
+import junit.framework.TestCase
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ReviewTest {
+
+    @Test
+    fun TasteTest() {
+       val r = Review(10,10,10);
+        assertEquals(r.taste, 10);
+    }
+    @Test
+    fun CostTest() {
+        val r = Review(10,10,10);
+        assertEquals(r.cost, 10);
+    }
+    @Test
+    fun AvailabilityTest() {
+        val r = Review(10,10,10);
+        assertEquals(r.availability, 10);
+    }
+}
