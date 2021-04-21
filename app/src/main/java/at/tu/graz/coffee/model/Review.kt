d44594d6f56dba8ea0e9e6583ac6b131a89552c1
John Tusha <john.tusha@student.tugraz.at>
2021-04-21 09:42:28 +0200

    COFFEE-005 Created Review-Class with Setters and test class [JT, TB]

--- app/src/main/java/at/tu/graz/coffee/model/Review.kt
+++ app/src/main/java/at/tu/graz/coffee/model/Review.kt
@@ -1,2 +1,18 @@
 package at.tu.graz.coffee.model
 
+class Review {
+    var taste: Int = 0
+    set(value) {
+        if (value in 0..10) field = value
+    }
+    var cost: Int = 0
+        set(value) {
+            if (value in 0..10) field = value
+        }
+    var availability:Int=0
+        set(value) {
+            if (value in 0..10) field = value
+        }
+
+
+}
