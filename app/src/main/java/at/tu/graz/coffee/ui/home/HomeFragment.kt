068b631b5eabd6734ddb0737adf7dd676595a856
Alexander Friessnig <a.friessnig@student.tugraz.at>
2021-04-24 13:30:29 +0200

    COFFEE-007-008-009-Remove redundant variables and code-[FA, PM]

--- app/src/main/java/at/tu/graz/coffee/ui/home/HomeFragment.kt
+++ app/src/main/java/at/tu/graz/coffee/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import at.tu.graz.coffee.R
 
@@ -21,11 +19,6 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_search, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 }
--- app/src/main/java/at/tu/graz/coffee/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/at/tu/graz/coffee/ui/slideshow/SlideshowFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import at.tu.graz.coffee.R
 
@@ -21,11 +19,6 @@ class SlideshowFragment : Fragment() {
     ): View? {
         slideshowViewModel =
                 ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_support, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_support, container, false)
     }
 }
