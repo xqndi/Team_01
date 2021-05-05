6d296098c9bbadcfb98aac92941cf33eb3d365c7
Laura Pessl <laura.pessl@student.tugraz.at>
2021-05-05 09:00:45 +0200

    COFFEE-015-018 created search, merged search & filter [FH, LP, MP, TB]

---
+++ app/src/main/java/at/tu/graz/coffee/ui/filter_result/FilterResultAdapter.kt
@@ -0,0 +1,65 @@
+package at.tu.graz.coffee.ui.filter_result
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.navigation.Navigation.findNavController
+import at.tu.graz.coffee.R
+import at.tu.graz.coffee.model.Coffee
+
+class FilterResultAdapter(context: Context, coffeeList:List<Coffee>): BaseAdapter() {
+    private val mContext: Context = context
+    private val mCoffeeList: List<Coffee> = coffeeList
+
+    override fun getCount(): Int {
+        return mCoffeeList.size
+    }
+
+    override fun getItem(p0: Int): Any {
+        return mCoffeeList[p0]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup): View {
+        val homeRow: View
+        if (convertView == null) {
+            val layoutInflater = LayoutInflater.from(mContext)
+            homeRow = layoutInflater.inflate(R.layout.home_row, viewGroup, false)
+            val nameTextView = homeRow.findViewById<TextView>(R.id.home_textView_name)
+            val ratingTextView = homeRow.findViewById<TextView>(R.id.home_textView_rating)
+            val imageView = homeRow.findViewById<ImageView>(R.id.home_imageView)
+            val ratingBar = homeRow.findViewById<RatingBar>(R.id.home_ratingBar)
+            val viewholder = ViewHolderPattern(nameTextView, ratingTextView, imageView, ratingBar)
+            homeRow.tag = viewholder
+        } else {
+            homeRow = convertView
+        }
+
+        homeRow.setOnClickListener {
+            val action = FilterResultFragmentDirections.actionOpenDetails(mCoffeeList[position].id)
+            findNavController(viewGroup).navigate(action)
+        }
+
+        val viewholder = homeRow.tag as ViewHolderPattern
+        viewholder.nameTextView.text = mCoffeeList[position].name
+        //TODO: rating is not implemented yet, so we use price to show the stars in the rating
+        viewholder.ratingTextView.text = mCoffeeList[position].price.toString()
+
+        viewholder.imageView.setImageResource(mContext.resources.getIdentifier(mCoffeeList[position].image,
+            "drawable", mContext.packageName))
+
+        viewholder.ratingBar.rating = mCoffeeList[position].price.toFloat()
+        return homeRow
+    }
+
+    private class ViewHolderPattern(val nameTextView: TextView, val ratingTextView: TextView,
+                                    val imageView: ImageView, val ratingBar: RatingBar)
+}
