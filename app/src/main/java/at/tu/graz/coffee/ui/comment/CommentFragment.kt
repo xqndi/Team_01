b70470db1394d1cebb351e82085e9920241a4806
John Tusha <john.tusha@student.tugraz.at>
2021-05-05 11:44:58 +0200

    COFFEE-014-Comments introduced sliders [JT,SL]

--- app/src/main/java/at/tu/graz/coffee/ui/comment/CommentFragment.kt
+++ app/src/main/java/at/tu/graz/coffee/ui/comment/CommentFragment.kt
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.navArgs
 import at.tu.graz.coffee.R
 import at.tu.graz.coffee.model.Review
 import at.tu.graz.coffee.ui.coffee_detail.CoffeeDetailFragmentArgs
+import com.google.android.material.slider.RangeSlider
 import java.util.*
 
 class CommentFragment : Fragment() {
@@ -43,17 +44,32 @@ class CommentFragment : Fragment() {
         reviewsList = (viewModel.getCoffee(args.coffeeId)?.reviews) as MutableList<Review>
 
         val listView: ListView = view.findViewById(R.id.comment_listview)
+
         listView.adapter = CommentAdapter(requireContext(), reviewsList)
         listAdapter = listView.adapter as CommentAdapter
 
         val submitButton = view.findViewById<Button>(R.id.btn_comment_submit)
         submitButton.setOnClickListener {
             val text = view.findViewById(R.id.comment_text_field) as EditText
-            val newReview = Review(10, 10, 10, text.text.toString())
+
+            val costSlider = view.findViewById(R.id.comment_cost) as RangeSlider
+            val valueCost : MutableList<Float> = costSlider.values
+
+            val tasteSlider = view.findViewById(R.id.comment_taste) as RangeSlider
+            val valueTaste : MutableList<Float> = tasteSlider.values
+
+            val availabilitySlider = view.findViewById(R.id.comment_availability) as RangeSlider
+            val valueAvailability : MutableList<Float> = availabilitySlider.values
+
+
+            val newReview = Review(valueTaste[0].toInt(), valueCost[0].toInt(), valueAvailability[0].toInt(), text.text.toString())
             coffee.reviews = (coffee.reviews.toMutableList() + newReview) as MutableList<Review>
 
             listView.adapter = CommentAdapter(requireContext(), coffee.reviews)
             text.text.clear()
+            costSlider.setValues(0.0F,0.0F)
+            tasteSlider.setValues(0.0F,0.0F)
+            availabilitySlider.setValues(0.0F,0.0F)
 
             Toast.makeText(activity, "Comment added", Toast.LENGTH_SHORT).show()
         }
