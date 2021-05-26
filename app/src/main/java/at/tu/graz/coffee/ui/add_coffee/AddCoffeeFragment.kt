0b9efb8db272a6db7171609fa0716b2af9acf7b1
Florian Haid <florian.haid@student.tugraz.at>
2021-05-26 10:21:45 +0200

    COFFEE-020 Fixed a bug and image can be opened [HF, TJ]

--- app/src/main/java/at/tu/graz/coffee/ui/add_coffee/AddCoffeeFragment.kt
+++ app/src/main/java/at/tu/graz/coffee/ui/add_coffee/AddCoffeeFragment.kt
@@ -1,10 +1,8 @@
 package at.tu.graz.coffee.ui.add_coffee
 
-import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,13 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import at.tu.graz.coffee.R
 import at.tu.graz.coffee.model.CoffeeType
 import kotlinx.android.synthetic.main.fragment_add_coffee.*
 import kotlinx.android.synthetic.main.fragment_filter_result.*
+import com.stfalcon.frescoimageviewer.ImageViewer
 
 
 class AddCoffeeFragment : Fragment() {
@@ -43,44 +41,51 @@ class AddCoffeeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val adapter = ArrayAdapter(
-            requireContext(), // Context
-            android.R.layout.simple_spinner_item, // Layout
-            CoffeeType.values() // Array
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            CoffeeType.values()
         )
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
 
         button_addCoffee.setOnClickListener{
+            var missingField = false
+
             if(coffee_name.text.isEmpty()) {
-                mandatory_field.visibility = View.VISIBLE
+                missingField = true
                 coffee_name.setBackgroundColor(Color.RED)
             }
             else {
-                mandatory_field.visibility = View.GONE
                 coffee_name.setBackgroundColor(Color.WHITE)
             }
+
             if(coffee_shop.text.isEmpty()) {
-                mandatory_field.visibility = View.VISIBLE
+                missingField = true
                 coffee_shop.setBackgroundColor(Color.RED)
             }
             else {
-                mandatory_field.visibility = View.GONE
                 coffee_shop.setBackgroundColor(Color.WHITE)
             }
+
             if(coffee_price.text.isEmpty()) {
-                mandatory_field.visibility = View.VISIBLE
+                missingField = true
                 coffee_price.setBackgroundColor(Color.RED)
             }
             else {
-                mandatory_field.visibility = View.GONE
                 coffee_price.setBackgroundColor(Color.WHITE)
             }
+
             if(imageView.drawable == null) {
-                mandatory_field.visibility = View.VISIBLE
+                missingField = true
                 button_addPicture.setBackgroundColor(Color.RED)
             }
             else {
-                mandatory_field.visibility = View.GONE
                 button_addPicture.setBackgroundColor(resources.getColor(R.color.purple_500))
+            }
+
+            if(missingField) {
+                mandatory_field.visibility = View.VISIBLE
+            } else {
+                mandatory_field.visibility = View.GONE
             }
         }
 
@@ -90,10 +95,8 @@ class AddCoffeeFragment : Fragment() {
                     startActivityForResult(phonePictures, pictureSelector)
         }
 
-        // Finally, data bind the spinner object with adapter
         spinner?.adapter = adapter;
 
-        // Set an on item selected listener for spinner object
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>,
@@ -101,15 +104,13 @@ class AddCoffeeFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                // Display the selected item text on text view
-                //text_view.text = "Spinner selected : ${parent.getItemAtPosition(position).toString()}"
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
-                // Another interface callback
             }
         }
     }
+
     override fun onActivityResult(neededPart: Int, outcomePart: Int, info: Intent?) {
         super.onActivityResult(neededPart, outcomePart, info)
 
@@ -117,6 +118,15 @@ class AddCoffeeFragment : Fragment() {
             if (outcomePart == RESULT_OK) {
                 uriPicture = info?.data
                 imageView.setImageURI(uriPicture)
+
+                val uri: MutableList<Uri> = ArrayList<Uri>()
+                uri.add(uriPicture!!)
+
+                imageView.setOnClickListener{
+                    ImageViewer.Builder(context, uri)
+                        .setStartPosition(0)
+                        .show()
+                }
             }
         }
     }
